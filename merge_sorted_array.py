224a17c633cdff5ea0638537faff92390bbe8a80
Kasra Namiranian <114209244+KasraNamiranian@users.noreply.github.com>
2024-07-05 00:43:29 +0330

    Update merge_sorted_array.py

--- merge_sorted_array.py
+++ merge_sorted_array.py
@@ -1,3 +1,5 @@
+#The answer of leetcode top 1nterview 150 the question number 88 merged sorted array written by Kasra Namiranian.
+
 class Solution:
     def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
         for i in range(n):

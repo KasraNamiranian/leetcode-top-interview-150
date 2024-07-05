1f0747cc793fcb94ef8dc4c5624a6aca7d2bd63f
Kasra Namiranian <114209244+KasraNamiranian@users.noreply.github.com>
2024-07-05 07:30:40 +0330

    here i write the merge_sorted_array1 with python.

---
+++ merge_sorted_array2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        n1p = m - 1
+        n2p = n - 1
+        wp = m + n -1
+
+        while(wp>=0):
+            if (n1p<0):
+                nums1[wp] = nums2[n2p]
+                wp -= 1
+                n2p -= 1
+            elif (n2p<0):
+                nums1[wp] = nums1[n1p]
+                wp -= 1
+                n1p -= 1
+            elif (nums1[n1p]>nums2[n2p]):
+                nums1[wp] = nums1[n1p]
+                wp -= 1
+                n1p -= 1
+            else:
+                nums1[wp] = nums2[n2p]
+                wp -=1
+                n2p -=1
+
+

4c065866b1013d02477ff747cb3193a6c49bc79f
Kasra Namiranian <114209244+KasraNamiranian@users.noreply.github.com>
2024-08-29 23:56:21 +0330

    Update majority_element1.rs

--- majority_element1.rs
+++ majority_element1.rs
@@ -1,3 +1,5 @@
+#Another answer of leetcode top 1nterview 150 the question number 169 (majority elements) written by Kasra Namiranian.
+
 use std::collections::HashMap;
 impl Solution {
     pub fn majority_element(nums: Vec<i32>) -> i32 {

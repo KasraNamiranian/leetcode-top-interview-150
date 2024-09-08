d129288954248a74d4e955d16d984f17ba06093b
Kasra Namiranian <114209244+KasraNamiranian@users.noreply.github.com>
2024-09-08 23:30:25 +0330

    Update length_of_last_word.c

--- length_of_last_word.c
+++ length_of_last_word.c
@@ -1,3 +1,5 @@
+#The answer of leetcode top 1nterview 150 the question number 58 (length of last word) written by Kasra Namiranian.
+
 int lengthOfLastWord(char* s) {
     int l = 0;
     int i = strlen(s);
